e48de779cbf460f29e5d457f5b4eb46a2849f7c1
meisterk <franz@kohnlehome.de>
2020-10-22 09:54:50 +0200

    Change switch to 1 line

--- wuerfelstatistik.js
+++ wuerfelstatistik.js
@@ -4,27 +4,13 @@ const treffer = [0, 0, 0, 0, 0, 0];
 // 6000mal würfeln
 for (let index = 0; index < 6000; index++) {
     const wuerfel = Math.floor(Math.random() * 6) + 1;
-    switch (wuerfel) {
-        case 1:
-            treffer[0] = treffer[0] + 1;
-            break;
-        case 2:
-            treffer[1] = treffer[1] + 1;
-            break;
-        case 3:
-            treffer[2] = treffer[2] + 1;
-            break;
-        case 4:
-            treffer[3] = treffer[3] + 1;
-            break;
-        case 5:
-            treffer[4] = treffer[4] + 1;
-            break;
-        case 6:
-            treffer[5] = treffer[5] + 1;
-            break;
-    }
+    treffer[wuerfel - 1] = treffer[wuerfel - 1] + 1;
 }
 
 // Ausgabe
-
+console.log('1er: ' + treffer[0]);
+console.log('2er: ' + treffer[1]);
+console.log('3er: ' + treffer[2]);
+console.log('4er: ' + treffer[3]);
+console.log('5er: ' + treffer[4]);
+console.log('6er: ' + treffer[5]);
