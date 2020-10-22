dadc92c34487cd786d3c2973646b7e81800a0e66
meisterk <franz@kohnlehome.de>
2020-10-22 10:32:27 +0200

    Add function

--- wuerfelstatistik.js
+++ wuerfelstatistik.js
@@ -2,11 +2,8 @@
 const anzahlWuerfe = 600000;
 const treffer = [0, 0, 0, 0, 0, 0];
 
-// 6000mal würfeln
-for (let index = 0; index < anzahlWuerfe; index++) {
-    const wuerfel = Math.floor(Math.random() * 6) + 1;
-    treffer[wuerfel - 1] = treffer[wuerfel - 1] + 1;
-}
+// 6000mal wuerfeln
+wuerfeln(6000);
 
 // Ausgabe in Konsole
 for (let index = 0; index < 6; index++) {
@@ -21,11 +18,23 @@ const dreier = document.querySelector('#dreier');
 const vierer = document.querySelector('#vierer');
 const fuenfer = document.querySelector('#fuenfer');
 const sechser = document.querySelector('#sechser');
+const buttonWuerfeln = document.querySelector('#button-wuerfeln');
 
-// Text zuweisen
-einser.textContent = treffer[0];
-zweier.textContent = treffer[1];
-dreier.textContent = treffer[2];
-vierer.textContent = treffer[3];
-fuenfer.textContent = treffer[4];
-sechser.textContent = treffer[5];
+// Event-Handler für Button
+buttonWuerfeln.addEventListener('click', wuerfeln);
+
+function wuerfeln() {
+    for (let index = 0; index < 6000; index++) {
+        console.log('TEST');
+        const wuerfel = Math.floor(Math.random() * 6) + 1;
+        treffer[wuerfel - 1] = treffer[wuerfel - 1] + 1;
+    }
+
+    // Text zuweisen
+    einser.textContent = treffer[0];
+    zweier.textContent = treffer[1];
+    dreier.textContent = treffer[2];
+    vierer.textContent = treffer[3];
+    fuenfer.textContent = treffer[4];
+    sechser.textContent = treffer[5];
+}
