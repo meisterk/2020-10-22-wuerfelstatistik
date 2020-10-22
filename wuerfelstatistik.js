ff7e94ff644de548e4291c5930e51080a6d275ac
meisterk <franz.kohnle@sabel.com>
2020-10-22 11:10:59 +0200

    Change order in JavaScript

--- wuerfelstatistik.js
+++ wuerfelstatistik.js
@@ -1,17 +1,4 @@
-// Daten
-const anzahlWuerfe = 600000;
-const treffer = [0, 0, 0, 0, 0, 0];
-
-// 6000mal wuerfeln
-wuerfeln(6000);
-
-// Ausgabe in Konsole
-for (let index = 0; index < 6; index++) {
-    console.log(`${index + 1}er: ${treffer[index]}`);
-}
-
-// Ausgabe im Browserfenster
-// JavaScript-Variablen für HTML-Elemente
+// 1. JavaScript-Variablen für HTML-Elemente
 const einser = document.querySelector('#einser');
 const zweier = document.querySelector('#zweier');
 const dreier = document.querySelector('#dreier');
@@ -20,17 +7,30 @@ const fuenfer = document.querySelector('#fuenfer');
 const sechser = document.querySelector('#sechser');
 const buttonWuerfeln = document.querySelector('#button-wuerfeln');
 
-// Event-Handler für Button
-buttonWuerfeln.addEventListener('click', wuerfeln);
+// 2. Daten (globale Variablen)
+const anzahlWuerfe = 600000;
+let treffer = [0, 0, 0, 0, 0, 0];
 
-function wuerfeln() {
-    for (let index = 0; index < 6000; index++) {
-        console.log('TEST');
+// 3. Start
+wuerfeln(anzahlWuerfe);
+ausgabe();
+
+// 4. Event-Handler für Button
+buttonWuerfeln.addEventListener('click', function () {
+    wuerfeln(anzahlWuerfe);
+    ausgabe();
+});
+
+// 5. Funktionen
+function wuerfeln(anzahl) {
+    treffer = [0, 0, 0, 0, 0, 0];
+    for (let index = 0; index < anzahl; index++) {
         const wuerfel = Math.floor(Math.random() * 6) + 1;
         treffer[wuerfel - 1] = treffer[wuerfel - 1] + 1;
     }
+}
 
-    // Text zuweisen
+function ausgabe() {
     einser.textContent = treffer[0];
     zweier.textContent = treffer[1];
     dreier.textContent = treffer[2];
